4137e879ea5c6871e2c5986c07b742994a433d3b
wu-yiqiang <wu_yiqiang@outlook.com>
2025-10-22 22:47:33 +0800

    优化设置

--- app/common/request/user.go
+++ app/common/request/user.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Register struct {
-	Name         string `form:"name" json:"name" binding:"required"`
-	NickName     string `form:"nick_name" json:"nick_name"`
-	Email        string `form:"email" json:"email" binding:"required,email"`
-	Password     string `form:"password" json:"password" binding:"required"`
-	Avatar       string `form:"avatar" json:"avatar"`
-	Mobile       string `form:"mobile" json:"mobile"`
-	DepartmentId uint   `form:"department_id" json:"department_id"`
-	PositionId   uint   `form:"position_id" json:"position_id"`
-	EmployDate   string `form:"employ_date" json:"employ_day"`
+	Name         string `form:"name" json:"name" example:"Sutter" binding:"required"`
+	NickName     string `form:"nick_name" example:"Sutter" json:"nick_name"`
+	Email        string `form:"email" json:"email" example:"sutter.wu@itforce-tech.com" binding:"required,email"`
+	Password     string `form:"password" json:"password"example:"1234@Abcd" binding:"required"`
+	Avatar       string `form:"avatar" example:"" json:"avatar"`
+	Mobile       string `form:"mobile" example:"13557080956" json:"mobile"`
+	DepartmentId uint   `form:"department_id" example:"1" json:"department_id"`
+	PositionId   uint   `form:"position_id" example:"1" json:"position_id"`
+	EmployDate   string `form:"employ_date" example:"2025-09-05" json:"employ_date"`
 }
 
 func (register Register) GetMessages() ValidatorMessages {
